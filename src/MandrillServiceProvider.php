ee4b01bc1ced2793749605df0db5b16e9e7d891b
paul.bardack <paul.bardak@gmail.com>
2015-11-03 17:06:53 +0200

    codestyle

--- src/MandrillServiceProvider.php
+++ src/MandrillServiceProvider.php
@@ -16,14 +16,15 @@ class MandrillServiceProvider extends ServiceProvider
     {
         $this->app->singleton('mandrill', function (Application $app) {
             $key = env('MANDRILL_KEY', false);
-            if(!$key){
+            if (!$key) {
                 throw new LumenMandrillException('set mandrill key for LumenMandrill');
             }
 
             return new Mandrill(
                 new \Drunken\Manager($app->make('db')->connection()->getMongoDB()),
                 $app->make('log'),
-                $key);
+                $key
+            );
         });
     }
 }
